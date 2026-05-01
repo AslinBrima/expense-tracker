6649f067a0525f1551a96974dab0e3375e8915a4
AslinBrima <aslinbrima22@gmail.com>
2026-05-01 21:00:08 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from yourapp import views
 from . import views
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('',views.home),
     path('', views.dashboard, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('signup/', views.signup_view, name='signup'),
